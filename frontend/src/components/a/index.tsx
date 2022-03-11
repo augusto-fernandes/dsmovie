81518ddf02daba3cae3cb0440affa428000f7ec6
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2022-03-11 00:14:37 -0300

    erro na pasta

--- frontend/src/components/a/index.tsx
+++ frontend/src/components/a/index.tsx
@@ -8,10 +8,10 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior">
+                    <a href="https://github.com/augusto-fernandes">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/augusto-fernandes</p>
                         </div>
                     </a>
                 </div>
